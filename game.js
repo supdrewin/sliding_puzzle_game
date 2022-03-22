791d1ce8863f5afc48b77053169b81beae368812
Supdrewin <supdrewin@outlook.com>
2022-03-22 05:03:53 +0000

    Tue Mar 22 05:03:53 UTC 2022

--- game.js
+++ game.js
@@ -328,7 +328,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('sliding_puzzle_game_bg.wasm', import.meta.url);
+        input = new URL('game_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
